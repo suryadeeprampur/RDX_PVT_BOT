2356eaaa5d7a65365bf99836d60a19f6372fd952
MD Ibrahim Arman <ibrahim.arman.khan08@gmail.com>
2025-06-19 00:28:43 +0530

    ## Fixed by @VoidZero_Dev ##

---
+++ plugins/start_&_cb.py
@@ -0,0 +1,88 @@
+
+import random
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, ForceReply, CallbackQuery
+from helper.database import jishubotz
+from config import Config, Txt  
+  
+
+@Client.on_message(filters.private & filters.command("start"))
+async def start(client, message):
+    user = message.from_user
+    await jishubotz.add_user(client, message)                
+    button = InlineKeyboardMarkup([
+        [InlineKeyboardButton('🔊 Updates', url='https://t.me/OnAnimeSeries'),
+        InlineKeyboardButton('♻️ Sᴜᴩᴩᴏʀᴛ', url='https://t.me/MadflixBotz')],
+        [InlineKeyboardButton('❤️‍🩹 About', callback_data='about'),
+        InlineKeyboardButton('🛠️ Help', callback_data='help')],
+        [InlineKeyboardButton("👨‍💻 Developer 🧑‍💻", url='https://t.me/VoidZero_Dev')]
+    ])
+    if Config.START_PIC:
+        await message.reply_photo(Config.START_PIC, caption=Txt.START_TXT.format(user.mention), reply_markup=button, quote=True)       
+    else:
+        await message.reply_text(text=Txt.START_TXT.format(user.mention), reply_markup=button, disable_web_page_preview=True, quote=True)
+   
+
+@Client.on_callback_query()
+async def cb_handler(client, query: CallbackQuery):
+    data = query.data 
+    if data == "start":
+        await query.message.edit_text(
+            text=Txt.START_TXT.format(query.from_user.mention),
+            disable_web_page_preview=True,
+            reply_markup = InlineKeyboardMarkup([
+                [InlineKeyboardButton('🔊 Updates', url='https://t.me/OnAnimeSeries'),
+                InlineKeyboardButton('♻️ Sᴜᴩᴩᴏʀᴛ', url='https://t.me/MadflixBotz')],
+                [InlineKeyboardButton('❤️‍🩹 About', callback_data='about'),
+                InlineKeyboardButton('🛠️ Help', callback_data='help')],
+                [InlineKeyboardButton("👨‍💻 Developer 🧑‍💻", url='https://t.me/VoidZero_Dev')]
+            ])
+        )
+    elif data == "help":
+        await query.message.edit_text(
+            text=Txt.HELP_TXT,
+            disable_web_page_preview=True,
+            reply_markup=InlineKeyboardMarkup([
+                [InlineKeyboardButton("🔒 Close", callback_data = "close"),
+                InlineKeyboardButton("◀️ Back", callback_data = "start")]
+            ])            
+        )
+    elif data == "about":
+        await query.message.edit_text(
+            text=Txt.ABOUT_TXT.format(client.mention),
+            disable_web_page_preview = True,
+            reply_markup=InlineKeyboardMarkup([
+                [InlineKeyboardButton("🔒 Close", callback_data = "close"),
+                InlineKeyboardButton("◀️ Back", callback_data = "start")]
+            ])            
+        )
+    elif data == "close":
+        try:
+            await query.message.delete()
+            await query.message.reply_to_message.delete()
+            await query.message.continue_propagation()
+        except:
+            await query.message.delete()
+            await query.message.continue_propagation()
+
+
+
+
+
+@Client.on_message(filters.private & filters.command(["donate", "d"]))
+async def donate(client, message):
+	text = Txt.DONATE_TXT
+	keybord = InlineKeyboardMarkup([
+        			[InlineKeyboardButton("🦋 Admin",url = "https://t.me/VoidZero_Dev"), 
+        			InlineKeyboardButton("✖️ Close",callback_data = "close") ]])
+	await message.reply_text(text = text,reply_markup = keybord)
+
+
+## Fixed by @VoidZero_Dev ##
+# Jishu Developer 
+# Don't Remove Credit 🥺
+# Telegram Channel @MadflixBotz
+# Backup Channel @JishuBotz
+# Developer @JishuDeveloper
+# Contact @MadflixSupport
+## Fixed by @VoidZero_Dev ##
